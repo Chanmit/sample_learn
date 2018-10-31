a70b382f47cd7957cf3462ae0ecf8b9826057b39
chanmit <chanmit.kpm@gmail.com>
2018-10-31 14:42:50 +0700

    Add Minitest Reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
